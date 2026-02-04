643646fe7d0481b5671cc8da2bb4f87fa8f3482b
Oscar Jones <oscarjjones0@gmail.com>
2026-02-04 13:55:55 +0000

    Added option for any named pdfs

--- src/pdf_to_image.py
+++ src/pdf_to_image.py
@@ -4,13 +4,25 @@ import datetime
 
 def convert_pdf_to_images():
 
-    os.makedirs("./output", exist_ok=True)
+    # Variables for where the directories will be located
+    input_directory = "./input"
 
-    images = convert_from_path("./input/sample.pdf")
+    # Array to store the .pdf locations in
+    pdfs_to_process = []
 
-    for i, image in enumerate(images):
-        file_name = f"./output/page_{i}.jpg"
-        
-        image.save(file_name, "JPEG")
+    # Find all the pdfs to process
+    for entry in os.scandir(input_directory):
+        if entry.is_file():
+            pdfs_to_process.append(entry.path)
+    
+    for current_file in pdfs_to_process:
 
-convert_pdf_to_images()
+        clean_name = os.path.splitext(os.path.basename(current_file))[0]
+
+        images = convert_from_path(current_file)
+
+        for i, image in enumerate(images):
+            file_name = f"./output/{clean_name}page_{i}.jpg"
+            
+            image.save(file_name, "JPEG")
+
