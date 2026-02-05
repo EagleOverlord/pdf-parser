3b21282246b07a9a0b1984ef4fd80fecf8eb96ab
alexporritt <alexjporritt@gmail.com>
2026-02-05 04:54:59 +0000

    fix: done some error handling and made it so the user can install their ollama model via the program

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
-import log
-
-log.initialise_log()
+from log import *
+initialise_log()
 
 import pdf_to_image
 import ocr
@@ -8,22 +7,22 @@ import os
 import datetime
 import time
 
-log.log(f"Imports successfull at: {datetime.datetime.now()}")
+log(f"Imports successfull at: {datetime.datetime.now()}")
 
 os.makedirs("./input", exist_ok=True)
 os.makedirs("./output", exist_ok=True)
 os.makedirs("./output/text", exist_ok=True)
 
-log.log(f"Created directories successfull at: {datetime.datetime.now()}")
+log(f"Created directories successfull at: {datetime.datetime.now()}")
 
 image_conversion_start = time.time()
 pdf_to_image.convert_pdf_to_images()
 image_conversion_end = time.time()
 
-log.log(f"Image conversion took: {image_conversion_end - image_conversion_start} seconds.")
+log(f"Image conversion took: {image_conversion_end - image_conversion_start} seconds.")
 
 ocr_start = time.time()
 ocr.ocr_images_ollama()
 ocr_end = time.time()
 
-log.log(f"OCR conversion took: {ocr_end - ocr_start} seconds.")
+log(f"OCR conversion took: {ocr_end - ocr_start} seconds.")
--- src/ocr.py
+++ src/ocr.py
@@ -1,10 +1,10 @@
 import lmstudio as lms
 import ollama
-from ollama import chat
+from ollama import ResponseError, chat
 from ollama import ChatResponse
 import os
 import datetime
-import log
+from log import *
 
 def ocr_images_lmstudio():
 
@@ -20,7 +20,7 @@ def ocr_images_lmstudio():
             images_to_process.append(entry.path)
 
     # Load the model that will be used
-    model = lms.llm("allenai/olmocr-2-7b")
+    model = lms.llm("allenai/olmocr-2-7b") # This imports the wrong quantization, I'll fix it in a bit
 
     for current_file in images_to_process:
         image_path = current_file
@@ -52,28 +52,37 @@ def ocr_images_ollama():
         if entry.is_file():
             images_to_process.append(entry.path)
     
-    for current_file in images_to_process: # Loops through the images and proccesses them all individually
+    try:
+    
+        for current_file in images_to_process: # Loops through the images and proccesses them all individually
 
-        response: ChatResponse = chat(
-            model=model_name,
-            messages=[
-            {
-                'role': 'user', # Set the role of the api
-                'content': 'Free OCR.', # Prompt
-                'images': [current_file] # Select the image
-            },
-        ],
-        options={
-            'num_ctx':8000, # Set the context length
-        }
-        )
+            response: ChatResponse = chat(
+                model=model_name,
+                messages=[
+                {
+                    'role': 'user', # Set the role of the api
+                    'content': 'Free OCR.', # Prompt
+                    'images': [current_file] # Select the image
+                },
+            ],
+            options={
+                'num_ctx':8000, # Set the context length
+            }
+            )
 
-        file_name_only = os.path.basename(current_file)
+            file_name_only = os.path.basename(current_file)
 
-        # Write the results to a .txt file named after the input
-        f = open(f"./output/text/{file_name_only}.txt", "w")
-        f.write("\n")
-        f.write(response['message']['content'])
-        f.close()
+            # Write the results to a .txt file named after the input
+            f = open(f"./output/text/{file_name_only}.txt", "w")
+            f.write("\n")
+            f.write(response['message']['content'])
+            f.close()
+
+    except ResponseError as re:
+        log(f"Error: {re}")
+        log(f"Attempting to install {model_name}...") # If the model is not found, it will install it and run the function again
+        os.system(f"ollama pull {model_name}") # I still need to find a way to log the install process, but this is a start
+        log("Installation complete. Retrying OCR...")
+        os.system("python src/main.py") # Rerun program. Beware of recursive variables!
 
     chat(model=model_name, messages =[], keep_alive=0)
